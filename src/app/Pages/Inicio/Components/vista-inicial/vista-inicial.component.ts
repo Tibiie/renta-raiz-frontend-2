57fd72959baaaf4c9c4bf828e9fdaa267dc448bc
Tibiie <yuraniecheverri8@gmail.com>
2025-04-02 10:46:06 -0500

    aaa

---
+++ src/app/Pages/Inicio/Components/vista-inicial/vista-inicial.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vista-inicial',
+  standalone: true,
+  imports: [],
+  templateUrl: './vista-inicial.component.html',
+  styleUrl: './vista-inicial.component.scss'
+})
+export class VistaInicialComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { VistaInicialComponent } from './Pages/Inicio/Components/vista-inicial/vista-inicial.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        redirectTo: 'vista-inicial',
+        pathMatch: 'full'
+    },
+
+    {
+        path: 'vista-inicial',
+        component: VistaInicialComponent
+
+    }
+];
