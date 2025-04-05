fb2a9732d24cd33ccf1189ded2da6bd1755d9eac
4rtemis-25 <4rtemis-25@users.noreply.github.com>
2025-04-04 22:13:51 -0500

    favicon y pageError

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./Pages/Inicio/inicio.routes').then((m) => m.InicioRoutingModule),
+  },
 
-    {
-        path: '',
-        loadChildren: () => import('./Pages/Inicio/inicio.routes').then(m => m.InicioRoutingModule)
-    }
-    
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
 ];
