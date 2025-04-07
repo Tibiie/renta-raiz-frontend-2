4722455c877c273e5d659349f43d4c3547426281
Tibiie <yuraniecheverri8@gmail.com>
2025-04-07 09:51:25 -0500

    aaa

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,13 @@
+const token = 'APP_PORTAL_RENTA_Wm1SaFptbGhhR1pyYUd4aGJHUm0=_YmYyZjMzNjAtMzcwMC00ZDJlLTgyNTktOTI5MzhlN2FhOGM1';
+
 export const environment = {
-    production: false,
-    baseUrl: 'http://157.173.210.108:9999/api/v1',
-  };
-  
+  production: false,
+  baseUrl: 'http://157.173.210.108:9999/api/v1',
+};
+
+export const conexionVault = {
+  production: true,
+  vaultToken: '',
+  vaultUrl: ''
+}
+
