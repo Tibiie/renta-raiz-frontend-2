02244489b44706dacde2a7ec31ce20da04116605
Tibiie <yuraniecheverri8@gmail.com>
2025-04-13 19:39:45 -0500

    AAA

--- src/app/Pages/Inicio/Components/nuestro-equipo/nuestro-equipo.component.ts
+++ src/app/Pages/Inicio/Components/nuestro-equipo/nuestro-equipo.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../../../shared/navbar/navbar.component";
 
 @Component({
   selector: 'app-nuestro-equipo',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './nuestro-equipo.component.html',
   styleUrl: './nuestro-equipo.component.scss'
 })
 export class NuestroEquipoComponent {
+
+  abrirPestana(url: string) {
+    window.open(url, '_blank');
+  }
 
 }
