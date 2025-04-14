93e3e06c2c5ff4d8349ac981e696b0e4d6971941
4rtemis-25 <4rtemis-25@users.noreply.github.com>
2025-04-13 19:38:43 -0500

    Loader añadido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { isPlatformBrowser } from '@angular/common';
 import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
+import { NgxUiLoaderModule } from 'ngx-ui-loader';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgxUiLoaderModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   router = inject(Router);
@@ -22,7 +23,7 @@ export class AppComponent implements OnInit {
         if (event instanceof NavigationEnd) {
           initFlowbite();
         }
-      });
-    }
-  }
+      });
+    }
+  }
 }
