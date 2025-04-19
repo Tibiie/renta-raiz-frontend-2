2ce67279a9641e8de95086e8d7d3bf0e2688eb83
DuHuDiCo <dudicodiaz@gmail.com>
2025-04-19 10:11:30 -0500

    commi

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,19 +1,19 @@
-export const token = 'APP_PORTAL_RENTA_Wm1SaFptbGhhR1pyYUd4aGJHUm0=_YmYyZjMzNjAtMzcwMC00ZDJlLTgyNTktOTI5MzhlN2FhOGM1';
+export const token =
+  'APP_PORTAL_RENTA_Wm1SaFptbGhhR1pyYUd4aGJHUm0=_YmYyZjMzNjAtMzcwMC00ZDJlLTgyNTktOTI5MzhlN2FhOGM1';
 
 export const environment = {
   production: false,
-  baseUrl: 'http://rentaraiz.io/api/v1',
+  baseUrl: 'https://apiportalrenta.duckdns.org/api/v1',
 };
 
 export const conexionVault = {
   production: true,
   vaultToken: '',
-  vaultUrl: ''
-}
+  vaultUrl: '',
+};
 
 export const Login = {
   tokenEncrypt: 'T0FIUlRBQkNTV0E=',
   ivEncrypted: 'VUJTUldLRE1BWQ==',
-  token: token
-}
-
+  token: token,
+};
