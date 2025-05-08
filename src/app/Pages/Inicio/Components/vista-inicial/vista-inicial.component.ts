9195082c0930bfd6d913c2673b5d5ff601a56cc9
4rtemis-25 <hernandez.gomez292020@gmail.com>
2025-05-08 10:35:47 -0500

    c

--- src/app/Pages/Inicio/Components/vista-inicial/vista-inicial.component.ts
+++ src/app/Pages/Inicio/Components/vista-inicial/vista-inicial.component.ts
@@ -210,19 +210,18 @@ export class VistaInicialComponent implements OnInit, AfterViewInit {
     // Verificamos si estamos en el navegador antes de acceder a window
     const isMobile =
       isPlatformBrowser(this.platformId) && window.innerWidth < 768;
+      const grupoSize = isMobile ? 3 : 4;
     console.log(isMobile);
-
-    const grupoSize = isMobile ? 3 : 4;
 
     this.aliadosPorGrupo = [];
 
     // Agrupamos los aliados
-    for (let i = 0; i < this.aliados.length; i += grupoSize) {
+    for (let i = 0; i <= this.aliados.length - grupoSize; i++) {
       this.aliadosPorGrupo.push(this.aliados.slice(i, i + grupoSize));
     }
-
     console.log(this.aliadosPorGrupo);
   }
+
 
   redirigirContactanos() {
     this.router.navigate(['/contacto']);
