fa5ce21a1911c6bb4dcbd36c6b62c03f5bb7f8ab
Tibiie <yuraniecheverri8@gmail.com>
2025-05-13 14:56:21 -0500

    aa

--- src/app/shared/envio-exitoso/envio-exitoso.component.ts
+++ src/app/shared/envio-exitoso/envio-exitoso.component.ts
@@ -16,7 +16,7 @@ export class EnvioExitosoComponent implements OnInit {
   ngOnInit(): void {
     setTimeout(() => {
       this.enviarWthatsapp();
-    }, 2000);
+    }, 1500);
   } enviarWthatsapp() {
 
     var param = this.router.snapshot.queryParamMap.get('urlInmueble');
