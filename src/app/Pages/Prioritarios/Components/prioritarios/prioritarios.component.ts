ec34a0af2a0cd9b748ea9077cc5cc99c45a8e2ee
DuHuDiCo <dudicodiaz@gmail.com>
2025-10-30 22:26:35 -0500

    ccommit

---
+++ src/app/Pages/Prioritarios/Components/prioritarios/prioritarios.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prioritarios',
+  standalone: true,
+  imports: [],
+  templateUrl: './prioritarios.component.html',
+  styleUrl: './prioritarios.component.scss'
+})
+export class PrioritariosComponent {
+
+}
