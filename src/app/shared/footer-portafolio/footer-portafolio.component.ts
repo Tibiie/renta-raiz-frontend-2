00a8b89c322d993e3712d37ab594ac2b6cfc3a18
DuHuDiCo <dudicodiaz@gmail.com>
2025-11-26 21:22:19 -0500

    commit

--- src/app/shared/footer-portafolio/footer-portafolio.component.ts
+++ src/app/shared/footer-portafolio/footer-portafolio.component.ts
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './footer-portafolio.component.scss'
 })
 export class FooterPortafolioComponent {
+   abrirPestana(url: string) {
+    window.open(url, '_blank');
+  }
 
 }
